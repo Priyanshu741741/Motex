6ffceb4a810236ed22792c1c20dd38ed1591773f
Priyanshu741741 <priyanshu74141@gmail.com>
2025-03-22 17:40:30 +0530

    herosection

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,23 @@
 import { styled } from '@mui/material/styles';
 
-const HeroImage = styled('img')({
+const HeroImage = styled('img')(({ theme }) => ({
   width: '100%',
   height: '100%',
   objectFit: 'cover',
   position: 'absolute',
   top: 0,
   left: 0,
-  opacity: 1,  // Changed from 0 to 1 to make image fully visible
+  opacity: 1,
   mixBlendMode: 'normal',
   filter: 'none',
-  zIndex: 0    // Ensure image stays behind the text
-});
+  zIndex: 0,
+  [theme.breakpoints.down('md')]: {
+    height: '50vh',
+    position: 'relative'
+  }
+}));
 
+export default HeroImage;
 <HeroImage
   src="/MotexFeb3.jpg"
   alt="Hero Background"
